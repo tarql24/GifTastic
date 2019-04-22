7bd8c25641d37e651a0460f77b9bbda651972adb
tarql24 <tarql24@gmail.com>
2019-04-21 19:22:31 -0700

    update js

--- assets/gif.js
+++ assets/gif.js
@@ -1,4 +1,3 @@
-// $(document).on("click", ".animal", displayAnimalsGifs);
 var topics = [
   "birds",
   "cat",
@@ -9,7 +8,7 @@ var topics = [
   "bear",
   "frog",
   "hawk",
-  "zebra",
+  "zibra",
   "seal",
   "killer whale"
 ];
@@ -29,7 +28,7 @@ function renderButtons() {
   // (this is necessary otherwise we will have repeat buttons)
   $("#buttons-view").empty();
 
-  // Looping through the array of animals
+  // Looping through the array of movies
   for (var i = 0; i < topics.length; i++) {
     // Then dynamicaly generating buttons for each movie in the array
     // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
@@ -45,7 +44,7 @@ function renderButtons() {
   }
 }
 
-// This function handles events where one button is clicked
+// This function handles events where add animal button is clicked
 $("#add-animal").on("click", function(event) {
   // Preventing the buttons default behavior when clicked (which is submitting a form)
   event.preventDefault();
@@ -54,87 +53,70 @@ $("#add-animal").on("click", function(event) {
     .val()
     .trim();
 
-  // Adding the movie from the textbox to our array
+  // Adding the animals to our array
   topics.push(newTopic);
   console.log(topics);
   // Calling renderButtons which handles the processing of our movie array
   renderButtons();
 });
 
-// Function for displaying the movie info
-// We're adding a click event listener to all elements with the class "movie"
-// We're adding the event listener to the document because it will work for dynamically generated elements
-// $(".movies").on("click") will only add listeners to elements that are on the page at that time
-// $(document).on("click", ".movie", alertMovieName);
-
 // Calling the renderButtons function to display the intial buttons
-// renderButtons();
+renderButtons();
 
 // Adding click event listen listener to all buttons
-function displayAnimalsGifs() {
-  $(".animal").on("click", function() {
-    $("#animal-pic").empty();
-    // Grabbing and storing the data-animal property value from the button
-    var animal = $(this).attr("data-name");
+$(".animal").on("click", function() {
+  $("#animal-pic").empty();
+  // Grabbing and storing the data-animal property value from the button
+  var animal = $(this).attr("data-name");
 
-    // Constructing a queryURL using the animal name
-    var queryURL =
-      "https://api.giphy.com/v1/gifs/search?q=" +
-      animal +
-      "&api_key=dc6zaTOxFJmzC&limit=10";
+  // Constructing a queryURL using the animal name
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    animal +
+    "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After data comes back from the request
-      .then(function(response) {
-        console.log(queryURL);
+  // Performing an AJAX request with the queryURL
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // After data comes back from the request
+    .then(function(response) {
+      console.log(queryURL);
 
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
+      console.log(response);
+      // storing the data from the AJAX request in the results variable
+      var results = response.data;
 
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
-          // Creating and storing a div tag
-          var animalDiv = $("<p>");
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
+        // Creating and storing a div tag
+        var animalDiv = $("<div>");
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<div>").text("Rating: " + results[i].rating);
 
-          // Creating and storing an image tag
-          var animalImage = $("<img data-state='still' class='gif'>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          animalImage.attr("src", results[i].images.fixed_height.url);
-          animalImage.attr(
-            "data-still",
-            results[i].images.fixed_height_still.url
-          );
-          animalImage.attr("data-animate", results[i].images.fixed_height.url);
-          animalImage.attr("class", "gif");
-          animalImage.attr("data-state", "still");
+        // Creating and storing an image tag
+        var animalImage = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+        animalImage.attr("src", results[i].images.fixed_height_still.url);
+        animalImage.attr("data-still", results[i].images.fixed_height.url);
+        animalImage.attr("data-animate", results[i].images.fixed_height.url);
+        animalImage.attr("class", "gif");
+        animalImage.attr("data-state", "still");
 
-          // Appending the paragraph and image tag to the animalDiv
-          animalDiv.append(animalImage);
-          animalDiv.append(p);
+        // Appending the paragraph and image tag to the animalDiv
+        animalDiv.append(animalImage);
+        animalDiv.append(p);
 
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        $("#animal-pic").prepend(animalDiv);
+      }
+    });
+});
 
-          $("#animal-pic").prepend(animalDiv);
-        }
-        //   $("#animal-pic").empty();
-      });
-  });
-}
-
-$(".gif").on("click", function() {
-  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+function imageChangeState() {
   var state = $(this).attr("data-state");
-  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-  // Then, set the image's data-state to animate
-  // Else set src to the data-still value
   if (state === "still") {
     $(this).attr("src", $(this).attr("data-animate"));
     $(this).attr("data-state", "animate");
@@ -142,10 +124,5 @@ $(".gif").on("click", function() {
     $(this).attr("src", $(this).attr("data-still"));
     $(this).attr("data-state", "still");
   }
-});
-
-// Adding a click event listener to all elements with a class of "animal"
-$(document).on("click", ".animal", displayAnimalsGifs);
-
-// Calling the renderButtons function to display the intial buttons
-renderButtons();
+}
+$(document).on("click", ".gif", imageChangeState);
